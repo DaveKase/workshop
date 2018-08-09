2c0f64bd3b7ff06bf8747ffb92e4823820dbcff1
DaveKase <taavikase@gmail.com>
2018-08-09 16:06:40 +0300

    Changed some stuff

--- Android/Workshop/app/src/main/java/ee/taavikase/workshop/Sender.java
+++ Android/Workshop/app/src/main/java/ee/taavikase/workshop/Sender.java
@@ -18,7 +18,7 @@ import java.net.URL;
 
 public class Sender extends AsyncTask<String, Void, String> {
     private static final String TAG = "Sender";
-    private static final String SERVER_URL = "http://192.168.1.173:8080/workshop/receiveClick";
+    private static final String SERVER_URL = "http://192.168.1.105:8080/workshop/receiveClick";
     private static final String USER_ID = "Taavi Kase";
     private static final String JSON_USER_ID = "user_id";
     private static final String JSON_BTN_NAME = "btn_name";
